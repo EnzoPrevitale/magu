02e34ed6e2723bf47e7ba376e0e8b1717d50da65
ENZO GABRIEL PREVITALE SILVA <u23209@academico.cotuca.unicamp.br>
2025-05-15 18:57:22 -0300

    magu

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import datetime
 import pandas as pd
 
-planilha = {}
-
+planilha = {
+    "Semana": [],
+    "Pagante": []
+}
+pagantes = ["Enzo", "Pedrão", "André", "Samuel", "Belai", "Pedrão", "Alex", "Antônio", "Comunitária"]
 inicio = datetime.datetime(2025,4,4)
+semana_inicio = int(inicio.strftime("%V"))
+ciclos = int(input("Digite a quantidade de ciclos que deseja verificar: ")) * len(pagantes)
 
-for i in range(8):
-    pagantes = ["Enzo", "André", "Samuel", "Belai", "Pedrão", "Alex", "Antônio", "Comunitária"]
-    semana_inicio = int(inicio.strftime("%V"))
-    data = datetime.datetime.now()
-    dia = data.strftime("%j")
-    quem_paga =
+for i in range(ciclos):
+    data = datetime.datetime.now() + datetime.timedelta(i * 7)
+    pagante = pagantes[(int(data.strftime("%V")) + 1 - semana_inicio) % len(pagantes)]
+    dia = f"{data.strftime('%d')}/{data.strftime('%m')}"
 
-    print(quem_paga)
+    planilha["Semana"].append(dia)
+    planilha["Pagante"].append(pagante)
+
+df = pd.DataFrame(planilha)
+print(df)
+df.to_excel("coquinha.xlsx", sheet_name="escala", index = False)
