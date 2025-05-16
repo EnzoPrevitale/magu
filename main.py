3821e229d67bca24ce68f5e1333676f954419e3d
ENZO GABRIEL PREVITALE SILVA <u23209@academico.cotuca.unicamp.br>
2025-05-16 20:28:29 -0300

    magu 5

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 def verificar_ciclos(quantidade):
     planilha = {
+        "Ciclo": [],
         "Semana": [],
         "Pagante": []
     }
@@ -11,9 +12,12 @@ def verificar_ciclos(quantidade):
     for i in range(quantidade):
         data = datetime.datetime.now() + datetime.timedelta(i * 7)
         data += datetime.timedelta(5 - int(data.strftime('%w')))
+
         pagante = PAGANTES[(int(data.strftime("%V")) - SEMANA_INICIO) % len(PAGANTES)]
         dia = f"{data.strftime('%d')}/{data.strftime('%m')}"
+        ciclo = (data - INICIO).days // (7 * len(PAGANTES)) + 1
 
+        planilha["Ciclo"].append(ciclo)
         planilha["Semana"].append(dia)
         planilha["Pagante"].append(pagante)
 
@@ -41,9 +45,23 @@ while True:
     elif opcao == 2:
         for num in range(len(PAGANTES)):
             print(f"[{num + 1}] - {PAGANTES[num]}")
-        nome = int(input("Escolha: "))
+
+        nome = PAGANTES[int(input("Escolha: ")) - 1]
         datas = verificar_ciclos(3 * len(PAGANTES))
-        datas_nome = datas[datas["Pagante"] == PAGANTES[nome - 1]]
+        datas_nome = datas[datas["Pagante"] == nome]
+
         print(datas_nome)
-        datas_nome.to_excel(f"coquinha_{PAGANTES[nome - 1]}.xlsx", sheet_name="escala", index=False)
+        datas_nome.to_excel(f"coquinha_{nome}.xlsx", sheet_name="escala", index=False)
+
+        proxima_semana = (datas_nome[datas_nome['Ciclo'] == min(list(datas_nome['Ciclo']))])["Semana"].values[0]
+        proximo_ciclo = (datas_nome[datas_nome['Ciclo'] == min(list(datas_nome['Ciclo']))])["Ciclo"].values[0]
+        valor_total_estimado = f"{((proximo_ciclo - 1) * 14):.2f}".replace('.', ',')
+
+        print(f"\n"
+              f"Informações de {nome}: \n"
+              f"Próxima semana: {proxima_semana}\n"
+              f"Cocas pagas: {proximo_ciclo - 1}\n"
+              f"Valor total estimado: R${valor_total_estimado}"
+              )
         break
+
