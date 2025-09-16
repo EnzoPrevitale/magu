893cd277f5894cf31fb510c4e233b004ae1f62a5
ENZO GABRIEL PREVITALE SILVA <u23209@academico.cotuca.unicamp.br>
2025-09-16 19:31:03 -0300

    CRUD

--- api/views.py
+++ api/views.py
@@ -33,3 +33,6 @@ def pagante_detail(request, pk):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        pagante.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
